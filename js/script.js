292451fb0cc45ad316e0ee23b415cd00390bbe52
ksukriti08 <ksukriti08@gmail.com>
2024-08-14 13:45:41 -0400

    added script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function openTrip(evt, tripName) {
+    var i, tabcontent, tablinks;
+
+    // Hide all tab contents
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the "active" class from all tabs
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the selected tab content and add the "active" class to the clicked tab
+    document.getElementById(tripName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
